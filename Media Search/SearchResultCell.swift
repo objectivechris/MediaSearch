5a32ae71f8c7147220751deba926cdd3e4785c82
Christopher Rene <objectivechris@gmail.com>
2017-03-26 22:16:49 -0400

    styled search bar

--- Media Search/SearchResultCell.swift
+++ Media Search/SearchResultCell.swift
@@ -13,9 +13,12 @@ class SearchResultCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var artworkImageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        let selectedView = UIView(frame: CGRect.zero)
+        selectedView.backgroundColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 0.5)
+        selectedBackgroundView = selectedView
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
